cac26f1079eac06b4e30e1d4c325e27c2037467b
gnagle14 <g.nagle14@gmail.com>
2016-08-21 19:33:13 -0400

    responsive logo swap

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-console.log('fuck');
+$(function(){
+
+
+  function headerResize() {
+      var $wndwWdth = $(window).width(),
+          $logoImg  = $('.logo img');
+
+      if ( $wndwWdth < 1170 ) {
+        // console.log('cahgne');
+        $logoImg.attr('src', 'assets/icons/logo.svg');
+      } else {
+        $logoImg.attr('src', 'assets/icons/logo-1.svg');
+      }
+  }
+
+
+  $(window).resize(headerResize);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
