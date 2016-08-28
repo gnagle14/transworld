caaa1ceec1e495795e6aee6ccc200b8e88c9f582
gnagle14 <g.nagle14@gmail.com>
2016-08-28 12:11:40 -0400

    finished resize and scroll actions for the mainHead

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 // http://stackoverflow.com/questions/641857/javascript-window-resize-event
-
 var addEvent = function(object, type, callback) {
     if (object === null || typeof(object) == 'undefined') return;
     if (object.addEventListener) {
@@ -11,19 +10,73 @@ var addEvent = function(object, type, callback) {
     }
 };
 
-var logoImg = document.querySelector('.logo img'),
-    bigWidth    = 1000;
+
+
+// -----------------------------------
+// Main Header RESIZE & SCROLL actions
+
+var logoImg   = document.querySelector('.logo img'),
+    mainHead  = document.querySelector('.mainHead'),
+    bigWidth  = 1000,
+    hideHead;
+
+
+// RESIZE actions
+function noTrans() {
+  if (mainHead.classList.contains('slide-up') ) {
+    mainHead.classList.add('no-transition');
+    mainHead.classList.remove('slide-up');
+    setTimeout(function(){
+      mainHead.classList.remove('no-transition');
+    }, 100);
+  }
+}
 
 function mainHeadResize() {
   var wndWidth = window.innerWidth;
 
   if ( wndWidth <= bigWidth ) {
     logoImg.src = 'assets/icons/tws_logo.svg';
+    hideHead = false;
   }
   else {
     logoImg.src = 'assets/icons/tws_logo-2.svg';
+    hideHead = true;
+    noTrans();
   }
-
-} mainHeadResize();
-
+}
+mainHeadResize();
 addEvent(window, "resize", mainHeadResize);
+
+
+// SCROLL actions
+var
+    previousTop  = 0,
+    currentTop   = 0,
+    scrollDelta  = 10,
+    scrollOffset = 200;
+
+function checkHead(currentTop) {
+  // scrolling Down
+  if (currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
+    mainHead.classList.add('slide-up');
+  }
+  // scrolling Up
+  else if (previousTop - currentTop > scrollDelta) {
+    mainHead.classList.remove('slide-up');
+  }
+}
+
+function mainHeadAction(currentTop) {
+  currentTop = document.body.scrollTop;
+  checkHead(currentTop);
+  previousTop = currentTop;
+}
+
+function mainHeadScroll() {
+  if ( hideHead === false ) {
+    mainHeadAction();
+  }
+}
+// mainHeadScroll();
+addEvent(document, "scroll", mainHeadScroll);
